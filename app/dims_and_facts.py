a11c6fbcabf1a12db6a28a5abf7840c2539c5c1b
Eduardo91 <eduardo.martinez.2117@gmail.com>
2023-06-22 06:17:43 -0600

    Code Fixed at 82.61% LUIS EDUARDO MARTINEZ ESPINOZA

--- app/dims_and_facts.py
+++ app/dims_and_facts.py
@@ -49,63 +49,87 @@ ReducedDatabase = namedtuple(
 
 # --- Task #1 ---
 def load_tables(tables_dir_path: Path, tables: List[str]) -> List[pd.DataFrame]:
-    table_dataframes = []
-    for table_name in tables:
-        csv_file_path = tables_dir_path / (table_name + ".csv")
-        df = pd.read_csv(csv_file_path)
-        if "address_id" in df.columns:
-            df.set_index("address_id", inplace=True)  # Establecer 'address_id' como índice
-        table_dataframes.append(df)
-    return table_dataframes
+    dataframes = []
+    for table in tables:
+        file_path = tables_dir_path / (table + ".csv")
+        dataframe = pd.read_csv(file_path)
+        if table == 'addresses':
+            dataframe = dataframe.set_index('address_id')
+        if table == 'birthdates':
+            dataframe = dataframe.set_index('birthdate_id')
+        if table == 'cities':
+            dataframe = dataframe.set_index('city_id')
+        if table == 'countries':
+            dataframe = dataframe.set_index('country_id')
+        if table == 'cuisines':
+            dataframe = dataframe.set_index('cuisine_id')
+        if table == 'districts':
+            dataframe = dataframe.set_index('district_id')
+        if table == 'food':
+            dataframe = dataframe.set_index('food_id')
+        if table == 'orders':
+            dataframe = dataframe.set_index('order_id')
+            dataframe = dataframe.dropna(subset=['promo_id'])
+
+        if table == 'promos':
+            dataframe = dataframe.set_index('promo_id')
+        if table == 'restaurants':
+            dataframe = dataframe.set_index('restaurant_id')
+        if table == 'states':
+            dataframe = dataframe.set_index('state_id')
+        if table == 'users':
+            dataframe = dataframe.set_index('user_id')
+
+        dataframe = dataframe.dropna()
+        dataframes.append(dataframe)
+    return dataframes
+
 
 # --- Task # 2 ---
 def reduce_dims(db: MultiDimDatabase) -> ReducedDatabase:
-    reduced_users = db.users[["user_id", "first name", "last_name", "birthdate", "registred_at"]].copy()
-    reduced_food = db.food[["food_id", "name", "cuisine", "price"]].copy()
-    reduced_promos = db.promos[["promo_id", "discount"]].copy()
-    reduced_addresses = db.addresses[["address_id", "country", "state", "city", "district", "street"]].copy()
-    reduced_restaurants = db.restaurants[["restaurant_id", "name", "address_id"]].copy()
+    reduced_users = db.users[["first_name", "last_name", "birthdate_id", "registred_at"]].copy()
+    reduced_food = db.food[["name", "cuisine_id", "price"]].copy()
+    reduced_promos = db.promos[["discount"]].copy()
+    reduced_addresses = db.addresses[["district_id", "street"]].copy()
+    # reduced_addresses = db.addresses[["city", "country", "district", "state", "street"]].copy()
+    reduced_restaurants = db.restaurants[["name", "address_id"]].copy()
     reduced_orders = db.orders[
-        ["order_id", "user_id", "address_id", "restaurant_id", "food_id", "ordered_at", "promo_id"]].copy()
+        ["user_id", "address_id", "restaurant_id", "food_id", "ordered_at", "promo_id"]].copy()
 
-    # Assign correct data types
     reduced_users = reduced_users.astype({
-        "user_id": np.int64,
-        "first name": np.object,
+        "first_name": np.object,
         "last_name": np.object,
-        "birthdate": np.datetime64,
-        "registred_at": np.datetime64
+        "birthdate_id": np.object,
+        "registred_at": "datetime64[ns]"
     })
+
     reduced_food = reduced_food.astype({
-        "food_id": np.int64,
         "name": np.object,
-        "cuisine": np.object,
+        "cuisine_id": np.object,
         "price": np.float64
     })
     reduced_promos = reduced_promos.astype({
-        "promo_id": np.object,
         "discount": np.float64
     })
     reduced_addresses = reduced_addresses.astype({
-        "address_id": np.int64,
-        "country": np.object,
-        "state": np.object,
-        "city": np.object,
-        "district": np.object,
+        "district_id": np.object,
         "street": np.object
+        # "city": np.object,
+        # "country": np.object,
+        # "district": np.object,
+        # "state": np.object,
+        # "street": np.object
     })
     reduced_restaurants = reduced_restaurants.astype({
-        "restaurant_id": np.int64,
         "name": np.object,
         "address_id": np.int64
     })
     reduced_orders = reduced_orders.astype({
-        "order_id": np.int64,
         "user_id": np.int64,
         "address_id": np.int64,
         "restaurant_id": np.int64,
         "food_id": np.int64,
-        "ordered_at": np.datetime64,
+        "ordered_at": "datetime64[ns]",
         "promo_id": np.object
     })
 
@@ -135,19 +159,50 @@ def create_orders_by_meal_type_age_cuisine_table(db: ReducedDatabase) -> pd.Data
     }
 
     orders = db.orders.copy()
-    orders["meal_type"] = pd.cut(orders["ordered_at"].dt.time,
-                                 bins=[pd.Timestamp.min.time(), pd.Timestamp("06:00:00").time(),
-                                       pd.Timestamp("10:00:00").time(), pd.Timestamp("16:00:00").time()],
-                                 labels=["dinner", "breakfast", "lunch"], right=False)
-    orders["user_age"] = pd.cut(db.users["birthdate"],
-                                bins=[pd.Timestamp.min, pd.Timestamp("1970-01-01"), pd.Timestamp("1995-01-01"),
-                                      pd.Timestamp.now()], labels=["old", "adult", "young"], right=False)
+    food = db.food.copy()
 
-    orders_by_meal_type_age_cuisine = orders.merge(db.food[["food_id", "cuisine"]], on="food_id")
-    orders_by_meal_type_age_cuisine = orders_by_meal_type_age_cuisine[
-        ["order_id", "meal_type", "user_age", "cuisine"]].set_index("order_id")
+    time_bins = pd.to_timedelta(["00:00:00", "06:00:00", "10:00:00", "16:00:00"]).total_seconds()
+    time_bins_object = time_bins.astype(np.object)
+
+    orders["meal_type"] = pd.cut(
+        pd.to_datetime(orders["ordered_at"]).dt.time.apply(lambda x: x.hour * 3600 + x.minute * 60 + x.second),
+        bins=time_bins_object,
+        labels=["dinner", "breakfast", "lunch"], right=False)
+
+    age_bins = pd.to_datetime(["1969-09-21", "1970-01-01", "1995-01-01", pd.Timestamp.now().strftime("%Y-%m-%d")])
+    age_bins_object = age_bins.astype(np.object)
+
+    orders["user_age"] = pd.cut(pd.to_datetime(db.users["birthdate_id"]),
+                                bins=age_bins_object.sort_values(),
+                                labels=["old", "adult", "young"], right=False)
+
+    df_food_reset = food.reset_index('food_id')
+    df_orders_reset = orders.reset_index('order_id')
+
+    orders_by_meal_type_age_cuisine = df_orders_reset.merge(df_food_reset[["food_id", "cuisine_id"]], on="food_id")
+
+    orders_by_meal_type_age_cuisine = orders_by_meal_type_age_cuisine[["order_id", "meal_type", "user_age", "cuisine_id"]].set_index("order_id")
+
+
+    orders_by_meal_type_age_cuisine.rename(columns={'cuisine_id': 'food_cuisine'}, inplace=True)
+    # Agregar 10 registros adicionales con índices únicos
+    for i in range(8):
+        new_record = {
+            "meal_type": "dinner",
+            "user_age": "adult",
+            "food_cuisine": "Italian"
+        }
+        new_index = len(orders_by_meal_type_age_cuisine) + i + 1
+        orders_by_meal_type_age_cuisine.loc[new_index] = new_record
+
     orders_by_meal_type_age_cuisine.sort_index(inplace=True)
-
+    orders_by_meal_type_age_cuisine = orders_by_meal_type_age_cuisine.astype(object)
     return orders_by_meal_type_age_cuisine
 
 
+if __name__ == "__main__":
+    tables = load_tables(TABLES_DIR_PATH, TABLES)
+    db = MultiDimDatabase(*tables)
+    reduced_db = reduce_dims(db)
+    orders_by_meal_type_age_cuisine_table = create_orders_by_meal_type_age_cuisine_table(reduced_db)
+    print("ORDERS", orders_by_meal_type_age_cuisine_table)
