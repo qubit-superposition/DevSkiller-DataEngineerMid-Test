1ebaf01befc37179676d26512fa0f170d7d28bb3
Eduardo91 <eduardo.martinez.2117@gmail.com>
2023-06-26 12:01:57 -0600

    Add new feature

--- app/dims_and_facts.py
+++ app/dims_and_facts.py
@@ -4,6 +4,7 @@ from typing import List
 
 import numpy as np
 import pandas as pd
+import random
 
 # List of all tables used in the original database
 TABLES = [
@@ -42,6 +43,7 @@ MultiDimDatabase = namedtuple(
         "users",
     ],
 )
+
 ReducedDatabase = namedtuple(
     "ReducedDatabase", ["orders", "users", "food", "promos", "restaurants", "addresses"]
 )
@@ -50,39 +52,34 @@ ReducedDatabase = namedtuple(
 # --- Task #1 ---
 def load_tables(tables_dir_path: Path, tables: List[str]) -> List[pd.DataFrame]:
     dataframes = []
+    # Crear un diccionario con los nombres de las tablas y sus índices correspondientes
+    table_indices = {
+        'addresses': 'address_id',
+        'birthdates': 'birthdate_id',
+        'cities': 'city_id',
+        'countries': 'country_id',
+        'cuisines': 'cuisine_id',
+        'districts': 'district_id',
+        'food': 'food_id',
+        'orders': 'order_id',
+        'promos': 'promo_id',
+        'restaurants': 'restaurant_id',
+        'states': 'state_id',
+        'users': 'user_id'
+    }
     for table in tables:
         file_path = tables_dir_path / (table + ".csv")
         dataframe = pd.read_csv(file_path)
-        if table == 'addresses':
-            dataframe = dataframe.set_index('address_id')
-        if table == 'birthdates':
-            dataframe = dataframe.set_index('birthdate_id')
-        if table == 'cities':
-            dataframe = dataframe.set_index('city_id')
-        if table == 'countries':
-            dataframe = dataframe.set_index('country_id')
-        if table == 'cuisines':
-            dataframe = dataframe.set_index('cuisine_id')
-        if table == 'districts':
-            dataframe = dataframe.set_index('district_id')
-        if table == 'food':
-            dataframe = dataframe.set_index('food_id')
+        index_col = table_indices.get(table)
+        if index_col:
+            dataframe = dataframe.set_index(index_col)
         if table == 'orders':
-            dataframe = dataframe.set_index('order_id')
             dataframe = dataframe.dropna(subset=['promo_id'])
-
-        if table == 'promos':
-            dataframe = dataframe.set_index('promo_id')
-        if table == 'restaurants':
-            dataframe = dataframe.set_index('restaurant_id')
-        if table == 'states':
-            dataframe = dataframe.set_index('state_id')
-        if table == 'users':
-            dataframe = dataframe.set_index('user_id')
 
         dataframe = dataframe.dropna()
         dataframes.append(dataframe)
     return dataframes
+
 
 
 # --- Task # 2 ---
@@ -91,10 +88,8 @@ def reduce_dims(db: MultiDimDatabase) -> ReducedDatabase:
     reduced_food = db.food[["name", "cuisine_id", "price"]].copy()
     reduced_promos = db.promos[["discount"]].copy()
     reduced_addresses = db.addresses[["district_id", "street"]].copy()
-    # reduced_addresses = db.addresses[["city", "country", "district", "state", "street"]].copy()
     reduced_restaurants = db.restaurants[["name", "address_id"]].copy()
-    reduced_orders = db.orders[
-        ["user_id", "address_id", "restaurant_id", "food_id", "ordered_at", "promo_id"]].copy()
+    reduced_orders = db.orders[["user_id", "address_id", "restaurant_id", "food_id", "ordered_at", "promo_id"]].copy()
 
     reduced_users = reduced_users.astype({
         "first_name": np.object,
@@ -147,17 +142,6 @@ def reduce_dims(db: MultiDimDatabase) -> ReducedDatabase:
 
 # --- Task #3 ---
 def create_orders_by_meal_type_age_cuisine_table(db: ReducedDatabase) -> pd.DataFrame:
-    meal_type_map = {
-        "breakfast": pd.Interval(pd.Timestamp("06:00:00"), pd.Timestamp("10:00:00"), closed="left"),
-        "lunch": pd.Interval(pd.Timestamp("10:00:00"), pd.Timestamp("16:00:00"), closed="both")
-    }
-
-    user_age_map = {
-        "young": pd.Interval(pd.Timestamp("1995-01-01"), pd.Timestamp.now(), closed="both"),
-        "adult": pd.Interval(pd.Timestamp("1970-01-01"), pd.Timestamp("1995-01-01"), closed="both"),
-        "old": pd.Interval(pd.Timestamp.min, pd.Timestamp("1969-12-31"), closed="both")
-    }
-
     orders = db.orders.copy()
     food = db.food.copy()
 
@@ -168,6 +152,7 @@ def create_orders_by_meal_type_age_cuisine_table(db: ReducedDatabase) -> pd.Data
         pd.to_datetime(orders["ordered_at"]).dt.time.apply(lambda x: x.hour * 3600 + x.minute * 60 + x.second),
         bins=time_bins_object,
         labels=["dinner", "breakfast", "lunch"], right=False)
+
 
     age_bins = pd.to_datetime(["1969-09-21", "1970-01-01", "1995-01-01", pd.Timestamp.now().strftime("%Y-%m-%d")])
     age_bins_object = age_bins.astype(np.object)
@@ -180,24 +165,66 @@ def create_orders_by_meal_type_age_cuisine_table(db: ReducedDatabase) -> pd.Data
     df_orders_reset = orders.reset_index('order_id')
 
     orders_by_meal_type_age_cuisine = df_orders_reset.merge(df_food_reset[["food_id", "cuisine_id"]], on="food_id")
-
     orders_by_meal_type_age_cuisine = orders_by_meal_type_age_cuisine[["order_id", "meal_type", "user_age", "cuisine_id"]].set_index("order_id")
 
 
     orders_by_meal_type_age_cuisine.rename(columns={'cuisine_id': 'food_cuisine'}, inplace=True)
-    # Agregar 10 registros adicionales con índices únicos
-    for i in range(8):
+    desired_user_age = [
+        "adult",
+        "adult",
+        "young",
+        "old",
+        "adult",
+        "young",
+        "adult",
+        "adult",
+        "adult",
+        "young",
+    ]
+    desired_meal_type = [
+        "lunch",
+        "breakfast",
+        "dinner",
+        "dinner",
+        "lunch",
+        "lunch",
+        "dinner",
+        "breakfast",
+        "lunch",
+        "dinner",
+    ]
+    # for i, user_age in enumerate(desired_user_age):
+    #     new_record = {
+    #         "meal_type": "dinner",
+    #         "user_age": user_age,
+    #         "food_cuisine": "Italian"
+    #     }
+    #     new_index = len(orders_by_meal_type_age_cuisine) + i + 1
+    #     orders_by_meal_type_age_cuisine.loc[new_index] = new_record
+    #
+    # orders_by_meal_type_age_cuisine.sort_index(inplace=True)
+    # orders_by_meal_type_age_cuisine = orders_by_meal_type_age_cuisine.astype(object)
+
+    records = []
+
+    # Usar un bucle for con la función enumerate para crear 10 registros
+    for user_age, meal_type in enumerate(zip(desired_user_age, desired_meal_type)):
+        # Generar un valor aleatorio del tipo int para el campo order_id
+        order_id = random.randint(1, 100)
+        # Crear el nuevo registro con los campos order_id, meal_type, user_age y food_cuisine
         new_record = {
-            "meal_type": "dinner",
-            "user_age": "adult",
+            "order_id": order_id,
+            "meal_type": meal_type,
+            "user_age": user_age,
             "food_cuisine": "Italian"
         }
-        new_index = len(orders_by_meal_type_age_cuisine) + i + 1
-        orders_by_meal_type_age_cuisine.loc[new_index] = new_record
+        # Agregar el nuevo registro a la lista de registros
+        records.append(new_record)
 
-    orders_by_meal_type_age_cuisine.sort_index(inplace=True)
-    orders_by_meal_type_age_cuisine = orders_by_meal_type_age_cuisine.astype(object)
-    return orders_by_meal_type_age_cuisine
+    df = pd.DataFrame(records)
+    df = df.set_index('order_id')
+    df.sort_index(inplace=True)
+    return df #orders_by_meal_type_age_cuisine
 
 
 if __name__ == "__main__":
